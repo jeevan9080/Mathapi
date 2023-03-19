ad8dccb6ff2eddf565157b68df90563f4f70b1db
jeevan9080 <r.jeevan100@gmail.com>
2023-03-20 00:43:56 +0530

    20.2.23

--- todotutorial/exercise.py
+++ todotutorial/exercise.py
@@ -3,3 +3,34 @@ from pydantic import BaseModel
 from typing import Optional, List
 
 
+class Users(BaseModel):
+    id : int
+    email: str                    
+    password: str
+    phone: str
+    city: str
+    state: str
+    country: str
+    status: str
+
+
+app = FastAPI()
+
+store_users=[]
+
+@app.get('/')
+async def home():
+    return{"Welcome this is the home page"}
+
+@app.post('/users/')
+async def create_users(users:Users):
+    store_users.append(users)
+    return users
+
+@app.get('/users/',response_model=List[Users])
+async def get_all_users():
+    return store_users
+
+
+
+
