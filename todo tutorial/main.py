183876aec347d7dfcfd5aee8fc3df14763114c13
jeevan9080 <r.jeevan100@gmail.com>
2023-03-19 20:16:28 +0530

    19.3.23

--- todo tutorial/main.py
+++ todo tutorial/main.py
@@ -1,3 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+
+class Todo(BaseModel):
+    name:str
+    due_date:str
+    description:str
+
+app=FastAPI(title="Todo API")
+
+#create read,update,delete
+
+store_todo=[]
+
+@app.get("/")
+async def home():
+    return {"Hello":"World"}
+
+@app.post("/todo/")
+async def create_todo(todo:Todo):
+    store_todo.append(todo)
+    return todo
+
+@app.get("/todo/", response_model=List[Todo])
+async def get_all_todos():
+    return store_todo
+
+@app.get("/todo/{id}")
+async def get_todo(id:int):
+    try:
+        return store_todo[id]
+    except:
+        raise HTTPException(status_code=404,detail="Todo Not Found")
