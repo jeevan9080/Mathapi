f09a1d3916875d8322d8d20189f89c4b1bed91db
jeevan9080 <r.jeevan100@gmail.com>
2023-03-19 19:50:25 +0530

    19.3.23

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 
 app=FastAPI()
 
-@app.get("/")
-def home():
-    return {"Data":"Test"}
+inventory = {
+        1:{
+            "name":"Milk",
+            "price":3.99,
+            "brand":"Regular"
+        }
 
-@app.get("/about")
-def about():
-    return{"Data":"About"}
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id:int=Path(None,description="The ID of the item")):
+    return inventory[item_id]
+
+@app.get("/get-by-name")
+def get_item(name:str=None):
+    for item_id in inventory:
+        if inventory[item_id]["name"]==name:
+            return inventory[item_id]
+    return{"Data":"Not Found"}
