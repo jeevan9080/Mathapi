0607f6cd6adf4102c6db7a033103df387afda304
jeevan9080 <r.jeevan100@gmail.com>
2023-03-20 15:34:27 +0530

    xzx

--- student/models.py
+++ student/models.py
@@ -4,7 +4,7 @@ from database import Base
 # Define To Do class inheriting from Base
 class student(Base):
     __tablename__ = 'student'
-    id = Column(String(256), primary_key=True)
+    id = Column(String(256))
     name = Column(String(256))
 
     
