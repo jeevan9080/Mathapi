6d69cb45efb5e4b2cf0d5bbb23ab0a5be141d34a
jeevan9080 <r.jeevan100@gmail.com>
2023-04-06 15:44:25 +0530

    ds

--- karishma/main.py
+++ karishma/main.py
@@ -11,7 +11,7 @@ Base.metadata.create_all(engine)
 # Initialize app
 app = FastAPI()
 
-# provides database session to each request upon calling
+# proves database session to each request upon calling
 def get_session():
     session = SessionLocal()
     try:
@@ -27,8 +27,10 @@ def root():
 def create_tax(tax: schemas.taxcreate, session: Session = Depends(get_session)):
 
     # create an instance of tax Model
-    tax_obj = models.tax(id=tax.id,
-    name=tax.name)
+    tax_obj = models.tax(Name=tax.Name,
+    Description=tax.Description,
+    Price=tax.Price,
+    GST=tax.GST)
 
     # Add the object into our database Table
     session.add(tax_obj)
@@ -44,43 +46,43 @@ def read_todo_list(session: Session = Depends(get_session)):
     tax_list = session.query(models.tax).all()
     return tax_list
     
-@app.get("/read/id{id}", response_model=schemas.taxall)
-def read_todo_id(id: str, session: Session = Depends(get_session)):
-    # Fetch todo record using id from the table    
-    tax_obj = session.query(models.tax).get(id)
-    # Check if there is record with the provided id, if not then Raise 404 Exception    
+@app.get("/read/Name{Name}", response_model=schemas.taxall)
+def read_todo_Name(Name: str, session: Session = Depends(get_session)):
+    # Fetch todo record using Name from the table    
+    tax_obj = session.query(models.tax).get(Name)
+    # Check if there is record with the provNameed Name, if not then Raise 404 Exception    
     if not tax_obj:
-        raise HTTPException(status_code=404, detail=f"tax item with id {id} not found")
+        raise HTTPException(status_code=404, detail=f"tax item with Name {Name} not found")
     return tax_obj 
 
-@app.put("/update/id{id}", response_model=schemas.taxall)
-def update_tax(id: str, name: str, session: Session = Depends(get_session)):
+@app.put("/update/Name{Name}", response_model=schemas.taxall)
+def update_tax(Name: str, name: str, session: Session = Depends(get_session)):
 
-    # Fetch tax record using id from the table
-    tax_obj = session.query(models.tax).get(id)
+    # Fetch tax record using Name from the table
+    tax_obj = session.query(models.tax).get(Name)
 
     # If the record is present in our DB table then update 
     if tax_obj:
-        tax_obj.id = id
+        tax_obj.Name = Name
         tax_obj.name = name
 
-    # if tax item with given id does not exists, raise exception and return 404 not found response
+    # if tax item with given Name does not exists, raise exception and return 404 not found response
     if not tax_obj:
-        raise HTTPException(status_code=404, detail=f"tax item with id {id} not found")
+        raise HTTPException(status_code=404, detail=f"tax item with name {Name} not found")
 
     return tax_obj
 
-@app.delete("/delete/id{id}", response_model = str)
-def delete_tax(id: str, session: Session = Depends(get_session)):
+@app.delete("/delete/Name{Name}", response_model = str)
+def delete_tax(Name: str, session: Session = Depends(get_session)):
 
-    # Fetch tax record using id from the table
-    tax_obj = session.query(models.tax).get(id)
+    # Fetch tax record using Name from the table
+    tax_obj = session.query(models.tax).get(Name)
 
     # Check if tax record is present in our Database,If not then raise 404 error
     if tax_obj:
         session.delete(tax_obj)
         session.commit()
     else:
-        raise HTTPException(status_code=404, detail=f"tax item with id {id} not found")
+        raise HTTPException(status_code=404, detail=f"tax item with Name {Name} not found")
 
-    return f"tax record with id {id} successfully deleted"
+    return f"tax record with Name {Name} successfully deleted"
--- karishma/models.py
+++ karishma/models.py
@@ -4,7 +4,7 @@ from database import Base
 # Define To Do class inheriting from Base
 class tax(Base):
     __tablename__ = 'tax'
-    Name: Column(String(256)) 
+    Name: Column(String(256),primary_key=True) 
     Description : Column(String(256))
     Price: Column(Integer())
     GST: Column(Integer())
