7f77223fc2ee9d9aedec56e7c1aea0c9df228d11
jeevan9080 <r.jeevan100@gmail.com>
2023-04-06 15:39:17 +0530

    dsd

--- karishma/main.py
+++ karishma/main.py
@@ -21,16 +21,14 @@ def get_session():
 
 @app.get("/")
 def root():
-    return "Welcome to tax Calculation. Built with FastAPI."
+    return "Welcome to tax Application. Built with FastAPI."
 
 @app.post("/create", response_model=schemas.taxall, status_code=status.HTTP_201_CREATED)
 def create_tax(tax: schemas.taxcreate, session: Session = Depends(get_session)):
 
     # create an instance of tax Model
-    tax_obj = models.tax(Name=tax.Name,
-    Description=tax.Description,
-    Price=tax.Price,
-    GST=tax.GST)
+    tax_obj = models.tax(id=tax.id,
+    name=tax.name)
 
     # Add the object into our database Table
     session.add(tax_obj)
@@ -40,26 +38,49 @@ def create_tax(tax: schemas.taxcreate, session: Session = Depends(get_session)):
     # return the tax object
     return tax_obj
 
+@app.get("/setup", response_model = List[schemas.taxall])
+def read_todo_list(session: Session = Depends(get_session)):
+    # get all todo items    
+    tax_list = session.query(models.tax).all()
+    return tax_list
     
-@app.get("/calculate", response_model=schemas.taxall)
-def read_todo_id(Name:str, Description : str, Price: int, GST: int, session: Session = Depends(get_session)):
+@app.get("/read/id{id}", response_model=schemas.taxall)
+def read_todo_id(id: str, session: Session = Depends(get_session)):
     # Fetch todo record using id from the table    
-    '''
     tax_obj = session.query(models.tax).get(id)
     # Check if there is record with the provided id, if not then Raise 404 Exception    
     if not tax_obj:
         raise HTTPException(status_code=404, detail=f"tax item with id {id} not found")
     return tax_obj 
-'''
-    thisdict = {
-        "Name": "Mobile",
-        "Description" : "Iphone 13 pro",
-        "Price": 100000,
-        "GST": 1800
-    }
-    return thisdict
 
-   
+@app.put("/update/id{id}", response_model=schemas.taxall)
+def update_tax(id: str, name: str, session: Session = Depends(get_session)):
 
+    # Fetch tax record using id from the table
+    tax_obj = session.query(models.tax).get(id)
 
+    # If the record is present in our DB table then update 
+    if tax_obj:
+        tax_obj.id = id
+        tax_obj.name = name
 
+    # if tax item with given id does not exists, raise exception and return 404 not found response
+    if not tax_obj:
+        raise HTTPException(status_code=404, detail=f"tax item with id {id} not found")
+
+    return tax_obj
+
+@app.delete("/delete/id{id}", response_model = str)
+def delete_tax(id: str, session: Session = Depends(get_session)):
+
+    # Fetch tax record using id from the table
+    tax_obj = session.query(models.tax).get(id)
+
+    # Check if tax record is present in our Database,If not then raise 404 error
+    if tax_obj:
+        session.delete(tax_obj)
+        session.commit()
+    else:
+        raise HTTPException(status_code=404, detail=f"tax item with id {id} not found")
+
+    return f"tax record with id {id} successfully deleted"
